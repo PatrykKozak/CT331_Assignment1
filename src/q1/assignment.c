1df1ffe048719f5157b2c2099221c643bb7f7c2d
PatrykKozak <patrykkozak1998@gmail.com>
2018-10-24 17:48:06 +0100

    q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+	int a;
+	int* b;
+	long c;
+	double* d;
+	char** e;
+
+	printf("a = %ld bytes \n", sizeof(a));
+	printf("b = %ld bytes \n", sizeof(b));
+	printf("c = %ld bytes \n", sizeof(c));
+	printf("d = %ld bytes \n", sizeof(d));
+	printf("e = %ld bytes \n", sizeof(e));
 }
